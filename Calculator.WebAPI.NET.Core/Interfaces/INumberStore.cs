9f7fd36495469d07fe7322403cae0b543277f69c
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-26 15:59:56 -0400

    Added INumberStore interface.

---
+++ Calculator.WebAPI.NET.Core/Interfaces/INumberStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.WebAPI.NET.Core.Interfaces
+{
+    public interface INumberStore
+    {
+        void StoreNumber(int number, string location, int position);
+        void ClearNumbers(string location);
+        (int? First, int? Second) GetNumbers(string location);
+    }
+}
