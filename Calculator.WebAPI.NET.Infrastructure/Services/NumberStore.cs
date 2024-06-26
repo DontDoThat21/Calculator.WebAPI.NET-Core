3a4f16a427633e5e2a211c3dd2d76e4bf2301cd5
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-26 16:05:40 -0400

    Added NumberStore service.

---
+++ Calculator.WebAPI.NET.Infrastructure/Services/NumberStore.cs
@@ -0,0 +1,45 @@
+﻿using Calculator.WebAPI.NET.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.WebAPI.NET.Infrastructure.Services
+{
+    public class NumberStore : INumberStore
+    {
+        private readonly Dictionary<string, (int? First, int? Second)> _store = new Dictionary<string, (int? First, int? Second)>();
+
+        public void StoreNumber(int number, string location, int position)
+        {
+            if (!_store.ContainsKey(location))
+            {
+                _store[location] = (null, null);
+            }
+
+            var currentNumbers = _store[location];
+            if (position == 1)
+            {
+                _store[location] = (number, currentNumbers.Second);
+            }
+            else if (position == 2)
+            {
+                _store[location] = (currentNumbers.First, number);
+            }
+        }
+
+        public void ClearNumbers(string location)
+        {
+            if (_store.ContainsKey(location))
+            {
+                _store[location] = (null, null);
+            }
+        }
+
+        public (int? First, int? Second) GetNumbers(string location)
+        {
+            return _store.ContainsKey(location) ? _store[location] : (null, null);
+        }
+    }
+}
