9daddc7e45a1d298396d07ebaae6bf32f408cbba
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-26 14:11:18 -0400

    Added tests project, and Controller for Calculator (wip)

---
+++ Calculator.WEBAPI.NET-Core.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Calculator.WEBAPI.NET_Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Calculator.WebAPI.NET-Core/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.WebAPI.NET_Core.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
