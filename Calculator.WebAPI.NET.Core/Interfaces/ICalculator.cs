57b51afc0b8d2db3ddb5466e778a28f007878283
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-26 15:43:00 -0400

    Added ICalculator interface.

---
+++ Calculator.WebAPI.NET.Core/Interfaces/ICalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.WebAPI.NET.Core.Interfaces
+{
+    public interface ICalculator
+    {
+        List<string> GetAvailableOperations(int firstNumber, int secondNumber);
+        double PerformCalculation(int firstNumber, int secondNumber, string operation);
+    }
+}
