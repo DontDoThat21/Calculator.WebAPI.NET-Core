aba01201e6a419179d235ce92d28739c38f18f97
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-26 15:31:06 -0400

    Finished calculator tests.

--- Calculator.WEBAPI.NET.Tests/UnitTests/CalculatorTests.cs
+++ Calculator.WEBAPI.NET.Tests/UnitTests/CalculatorTests.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Calculator.WebAPI.NET.Core.Interfaces;
 
 namespace Calculator.WebAPI.NET.Tests.UnitTests
 {
-    internal class CalculatorTests
+    public class CalculatorTests
     {
+        private readonly ICalculator _calculator;
+
+        public CalculatorTests()
+        {
+            _calculator = new Infrastructure.Services.Calculator();
+        }
+
+        /// <summary>
+        /// tests whether the add/sub/mult/div operation is returning a correct result
+        /// </summary>
+        [Theory]
+        [InlineData(9, 3, "Add", 12)]
+        [InlineData(5, 3, "Subtract", 2)]
+        [InlineData(50, 3, "Multiply", 150)]
+        [InlineData(40, 2, "Divide", 20)]
+        public void PerformCalculation_ShouldReturnCorrectResult(int firstNumber, int secondNumber, string operation, double expectedResult)
+        {
+            var result = _calculator.PerformCalculation(firstNumber, secondNumber, operation);
+            Assert.Equal(expectedResult, result);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void PerformCalculation_DivideByZero_ShouldThrowInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => _calculator.PerformCalculation(5, 0, "Divide"));
+        }
+
+        [Fact]
+        public void GetAvailableOperations_WithZeroSecondNumber_ShouldNotIncludeDivide()
+        {
+            var operations = _calculator.GetAvailableOperations(5, 0);
+            Assert.DoesNotContain("Divide", operations);
+        }
+
+        [Fact]
+        public void GetAvailableOperations_WithNonZeroSecondNumber_ShouldIncludeDivide()
+        {
+            var operations = _calculator.GetAvailableOperations(5, 3);
+            Assert.Contains("Divide", operations);
+        }
     }
 }
