c3b9ac2d9d004e86afd35c9554b08bdcbfb17eed
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-26 15:10:13 -0400

    Removed interfaces from API in prep. for a better SOC approach for using a .Core seperated project.

--- Calculator.WebAPI.NET.API/Interfaces/ICalculator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Calculator.WebAPI.NET.Core.Interfaces
-{
-    public interface ICalculator
-    {
-        List<string> GetAvailableOperations();
-        double PerformCalculation(string operation);
-    }
-}
--- Calculator.WebAPI.NET.API/Interfaces/INumberStore.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Calculator.WebAPI.NET.Core.Interfaces
-{
-    public interface INumberStore
-    {
-        void StoreNumber(int number, string location, int position);
-        void ClearNumbers(string location);
-        (int? First, int? Second) GetNumbers(string location);
-    }
-}
